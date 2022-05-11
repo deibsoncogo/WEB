04f417da8087e949f52747f72a7a25aaacf62514
VictorGuiVasco <victorguilhermecavalcante@gmail.com>
2022-05-11 12:11:26 -0300

    feat: api integration

--- src/layout/components/forms/forgot-password.tsx
+++ src/layout/components/forms/forgot-password.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useRouter } from 'next/router'
 
 import * as Yup from 'yup'
@@ -6,10 +6,14 @@ import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 
 import { Input } from '../inputs'
+import { api } from '../../../services/api'
 
 export function FormForgotPassword() {
   const router = useRouter()
   const formRef = useRef<FormHandles>(null)
+
+  const [hasError, setHasError] = useState(false)
+  const [message, setMessage] = useState('')
 
   async function handleFormSubmit(data: any) {
     if (!formRef.current) throw new Error()
@@ -21,7 +25,7 @@ export function FormForgotPassword() {
       })
       await schema.validate(data, { abortEarly: false })
 
-      router.push('/')
+      handleSendRequest(data)
     } catch (err) {
       const validationErrors = {}
       if (err instanceof Yup.ValidationError) {
@@ -34,8 +38,29 @@ export function FormForgotPassword() {
     }
   }
 
+  async function handleSendRequest(data: any) {
+    setHasError(false)
+    try {
+      const response = await api.post('/auth/forgotPassword', data)
+      router.push('/')
+    } catch (err: any) {
+      setHasError(true)
+      if (err.response.status === 500) {
+        setMessage(err.message)
+        return
+      }
+      setMessage(err.response.data.message[0])
+    }
+  }
+
   return (
     <Form className='form w-100' ref={formRef} onSubmit={handleFormSubmit}>
+      {hasError && (
+        <div className='alert alert-danger d-flex align-items-center p-5 mb-10'>
+          <span>{message}</span>
+        </div>
+      )}
+
       <Input name='email' label='E-mail' placeholder='E-mail' type='email' />
 
       <div className='mb-10 d-flex justify-content-between '>
