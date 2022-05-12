de1f1401b50e404423546d9872bad8bd1d47e1bf
Solenir <solenir.figueredo@gmail.com>
2022-05-11 21:47:45 -0300

    feat: added address api response

---
+++ src/interfaces/forms/api-response/address-response.ts
@@ -0,0 +1,11 @@
+export interface IAddressResponse {
+  id?: string;
+  userId?: string;
+  zipCode: string;
+  street: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  number: string;
+  complement: string;
+}
