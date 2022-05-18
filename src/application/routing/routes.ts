8fefe7a00be119a86f672a58fd5bdddb1f285010
Solenir <solenir.figueredo@gmail.com>
2022-05-18 01:01:08 -0300

    feat: included new routes

--- src/application/routing/routes.ts
+++ src/application/routing/routes.ts
@@ -1,14 +1,23 @@
 export const appRoutes = {
     LOGIN_PAGE : "/" ,
-    FORGOT_PASSWORD : "/forgetPassword",
-    RESET_PASSWORD : "/resetPassword",
-    USERS : "/users",
+    FORGOT_PASSWORD : "/password",
+    RESET_PASSWORD : "/reset-password", 
+    DASHBOARD: "/dashboard",    
     CATEGORIES : "/categories",
     CONTENTS : "/contents",
     BANNERS : "/banners",
     ALERTS : "/alerts", 
-    PROFESSOR: "/teacher",
-    DASHBOARD: "/dashboard",  
+    PLANNS: "/planns",
+    COURSES: "/courses",
+    BOOKS: "/books",
+    TRAININGS: "/trainings",
+    ROOMS: "/rooms",
+    COUPONS: "/coupons",
+    SALES: "/sales",    
+    USERS : "/users", 
+    LOGOUT: "/logout",
+    UNAUTHORIZED: "/unauthorized"
+
 }
 
 export const unprotectedRoutes = [appRoutes.LOGIN_PAGE,
@@ -16,14 +25,12 @@ export const unprotectedRoutes = [appRoutes.LOGIN_PAGE,
                                   appRoutes.RESET_PASSWORD]
 
 
-export const professorRoutes = [appRoutes.PROFESSOR,
-                                appRoutes.DASHBOARD]
+export const professorRoutes = [appRoutes.COURSES,
+                                appRoutes.BOOKS]
 
-export const adminRoutes = [professorRoutes,
-                            appRoutes.USERS,
+export const adminRoutes = [appRoutes.USERS,
                             appRoutes.CATEGORIES,
                             appRoutes.CONTENTS,
                             appRoutes.BANNERS,
-                            appRoutes.BANNERS     
-
-                           ]
+                            appRoutes.BANNERS
+                           ].concat(professorRoutes)
