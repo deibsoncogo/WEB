b4107964074023c3c6340662ec7d1f730e6332dc
Solenir <solenir.figueredo@gmail.com>
2022-05-26 00:41:19 -0300

    refactor: adjusted to recieve delete function

--- src/application/factories/components/course/courseTable-factory.tsx
+++ src/application/factories/components/course/courseTable-factory.tsx
@@ -1,7 +1,8 @@
 import CoursesTable from "../../../../layout/components/tables/courses-list"
+import { makeRemoteDeleteCourse } from "../../usecases/course/remote-deleteCourse-factory"
 import { makeRemoteGetAllCourses } from "../../usecases/course/remote-getAllCourses-factory"
 
 export const MakeCourseTable = () => {
-    return <CoursesTable getAllCourses={makeRemoteGetAllCourses()} />
+    return <CoursesTable getAllCourses={makeRemoteGetAllCourses()} deleteCourse = {makeRemoteDeleteCourse()}/>
   }
   
