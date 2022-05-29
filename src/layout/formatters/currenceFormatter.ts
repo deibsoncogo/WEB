76ca6466c6ef7e255aaf0257d545cb2ba3cc3f3e
Solenir <solenir.figueredo@gmail.com>
2022-05-29 17:48:11 -0300

    feat: added function to format currency without symbol real

--- src/layout/formatters/currenceFormatter.ts
+++ src/layout/formatters/currenceFormatter.ts
@@ -4,4 +4,12 @@ export function currenceMask (value: string) {
     }        
     return parseFloat(value).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
   }
+
+
+  export function currenceMaskOnlyValue (value?: string) {
+    if (!value) {
+      return '';
+    }        
+    return parseFloat(value).toLocaleString('pt-br', {minimumFractionDigits: 2});
+  }
   
