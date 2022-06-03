1e6e8708671bd4d67fbe89099e816f76b6780153
Solenir <solenir.figueredo@gmail.com>
2022-06-03 00:14:42 -0300

    feat: added class to represent courseClass

---
+++ src/domain/models/courseClass.ts
@@ -0,0 +1,13 @@
+export class CourseClass {
+  name: string;
+  link: string;
+  displayOrder: number;
+
+
+  constructor(name: string, link: string, displayOrder: number){
+      this.name = name;
+      this.link = link;
+      this.displayOrder = displayOrder
+
+  }
+}
