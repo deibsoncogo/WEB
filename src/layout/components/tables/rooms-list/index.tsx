ebdcc96a41f3757fba297a20919d88c003c2906f
Gustavo França <gstvz.franca@gmail.com>
2022-06-03 15:57:44 -0300

    chore: removed useRef import from rooms-list

--- src/layout/components/tables/rooms-list/index.tsx
+++ src/layout/components/tables/rooms-list/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Row } from './row'
 import { KTSVG } from '../../../../helpers'
