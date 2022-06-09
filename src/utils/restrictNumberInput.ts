5439bb28ba2001676397192699c7f680ec8ddbef
Gustavo França <gstvz.franca@gmail.com>
2022-06-09 11:40:05 -0300

    refactor: adjusted restrictNumberInput if check

--- src/utils/restrictNumberInput.ts
+++ src/utils/restrictNumberInput.ts
@@ -1,5 +1,4 @@
 export function restrictNumberInput(e: React.KeyboardEvent<HTMLInputElement>) {
-  if (e.key === 'Backspace' || 'Delete' || 'ArrowLeft' || 'ArrowRight') return;
+  if (e.key === 'Backspace' || e.key === 'Delete' || e.key ==='ArrowLeft' || e.key ==='ArrowRight') return;
   return e.key.match(/[^0-9]/g) && e.preventDefault();
-  // return ['e', 'E', '+', '-', ',', '.'].includes(e.key) && e.preventDefault()  
 }
