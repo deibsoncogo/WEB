82424967610cf7ef41c64befb4382901519ae7f2
Solenir <solenir.figueredo@gmail.com>
2022-06-11 17:16:39 -0300

    feat: added initial page to create new room

---
+++ src/pages/rooms/create.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { AsideDefault } from '../../layout/components/aside/AsideDefault'
+import { HeaderWrapper } from '../../layout/components/header/HeaderWrapper'
+
+
+const CreateRoom: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Criar Sala</title>
+      </Head>
+
+      <div className='page d-flex flex-row flex-column-fluid'>
+        <AsideDefault />
+
+        <div className='wrapper d-flex flex-column flex-row-fluid' id='kt_wrapper'>
+          <HeaderWrapper title='Nova Sala' />
+
+          <div id='kt_content_container' className='container'>
+            <div className=' bg-white rounded shadow-sm p-10 p-lg-15 mx-auto'>
+              {/* {MakeFormCreateCourse()} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CreateRoom
