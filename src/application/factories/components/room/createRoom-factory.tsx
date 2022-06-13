c7600484711c784e742ffbd1d0343bafe55d4452
Solenir <solenir.figueredo@gmail.com>
2022-06-13 20:43:03 -0300

    feat: added factory to createRoom

--- src/application/factories/components/room/createRoom-factory.tsx
+++ src/application/factories/components/room/createRoom-factory.tsx
@@ -2,10 +2,12 @@ import { FormCreateRoom } from "../../../../layout/components/forms/room/create/
 import { makeRemoteGetCategories } from "../../usecases/categories/remote-getCategories-factory";
 
 import { makeRemoteGetAllUsers } from "../../usecases/remote-getAllUsers-factory";
+import { makeRemoteCreateRoom } from "../../usecases/room/remote-createRoom-factory";
 
 
 export const MakeFormCreateRoom = () => {
-    return (<FormCreateRoom 
+    return (<FormCreateRoom
+     createRoom = {makeRemoteCreateRoom()}
      getCategories = {makeRemoteGetCategories()}
      getUsers = {makeRemoteGetAllUsers()}/> );
   };
