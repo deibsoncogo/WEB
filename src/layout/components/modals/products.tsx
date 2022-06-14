27205d856843694921898122dc25593b07375e3e
Gustavo França <gstvz.franca@gmail.com>
2022-06-14 15:47:14 -0300

    feat: started implementing a Form inside the modal, so the admin can add the free product to the user by submitting its data

--- src/layout/components/modals/products.tsx
+++ src/layout/components/modals/products.tsx
@@ -1,5 +1,9 @@
+import * as Yup from 'yup'
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web'
+import { useRef, useState } from 'react'
 import Modal from 'react-modal'
-import { KTSVG } from '../../../helpers'
+import { formatDateToUTC, KTSVG } from '../../../helpers'
 import { IPartialProductResponse } from '../../../interfaces/api-response/productsPartialResponse'
 import { DatePicker, Select } from '../inputs'
 
@@ -13,6 +17,46 @@ type NewTransactionModalProps = {
 }
 
 export function ProductsModal({ isOpen, modalTitle, message, action, onRequestClose, onAddProduct }: NewTransactionModalProps) {
+  const formRef = useRef<FormHandles>(null)
+
+  const [defaultValue, setDefaultValue] = useState({})
+
+  async function handleGrantProduct(data: IPartialProductResponse) {    
+    if (!formRef.current) throw new Error()   
+
+    try {      
+      formRef.current.setErrors({})
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é Necessário'),
+        expireDate: Yup.string().required('Data de nascimento é necessária')
+      })
+      await schema.validate(data, { abortEarly: false })
+      console.log(data)
+
+      const dataToSend = formatDataToSend(data)
+      // handleCreateUser(dataToSend)
+    } catch (err) {
+      const validationErrors = {}
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          // @ts-ignore
+          validationErrors[error.path] = error.message
+        })
+        formRef.current.setErrors(validationErrors)
+      }
+    }
+  }
+
+  function formatDataToSend(data: any) {
+    const userData = {
+      id,
+      name: data.name,
+      expireDate: formatDateToUTC(data.birthDate).toISOString().split('T')[0],
+    }      
+
+    return userData
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -34,18 +78,20 @@ export function ProductsModal({ isOpen, modalTitle, message, action, onRequestCl
           </div>
 
           <div className='modal-body'>
-            <div className='d-flex align-items-center gap-20'>
-              <div className='w-25'>
-                <Select name='courses' label='Cursos'>
-                  <option value='Curso 1'>
-                    Curso 1
-                  </option>
-                </Select>
-              </div>
-              <DatePicker name='expireDate' label='Data de expiração' />
-              <button type='button' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75' onClick={onRequestClose}>
-                + Adicionar outro curso
-              </button>
+            <div>
+              <Form className='form d-flex align-items-center gap-20 w-100' ref={formRef} initialData={defaultValue} onSubmit={handleGrantProduct}>
+                <div className='w-25'>
+                  <Select name='courses' label='Cursos'>
+                    <option value='Curso 1'>
+                      Curso 1
+                    </option>
+                  </Select>
+                </div>
+                <DatePicker name='expireDate' label='Data de expiração' />
+                <button type='submit' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75'>
+                  + Adicionar outro curso
+                </button>
+              </Form>
             </div>
             <div className='d-flex align-items-center gap-20'>
               <div className='w-25'>
@@ -56,7 +102,7 @@ export function ProductsModal({ isOpen, modalTitle, message, action, onRequestCl
                 </Select>
               </div>
               <DatePicker name='expireDate' label='Data de expiração' />
-              <button type='button' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75' onClick={onRequestClose}>
+              <button type='button' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75'>
                 + Adicionar outro treinamento
               </button>
             </div>
@@ -69,7 +115,7 @@ export function ProductsModal({ isOpen, modalTitle, message, action, onRequestCl
                 </Select>
               </div>
               <DatePicker name='expireDate' label='Data de expiração' />
-              <button type='button' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75' onClick={onRequestClose}>
+              <button type='button' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75'>
                 + Adicionar outro plano
               </button>
             </div>
@@ -79,7 +125,7 @@ export function ProductsModal({ isOpen, modalTitle, message, action, onRequestCl
             <button type='button' className='btn btn-primary' onClick={action}>
               Adicionar
             </button>
-            <button type='button' className='btn btn-light' onClick={onRequestClose}>
+            <button type='button' className='btn btn-light'>
               Cancelar
             </button>
           </div>
