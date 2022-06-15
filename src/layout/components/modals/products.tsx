da7c94c90938658cc0b56f609327239ef84a86db
Gustavo França <gstvz.franca@gmail.com>
2022-06-15 10:50:10 -0300

    feat: implemented logic to show selected products before granting them to the user; Refactored the logic to dynamic set the options on the Select inputs; Passed fixedValue prop to Select, so it the name of the product on the disabled input; Removed logic to show option with an error or 'Selecione' in the DatePicker

--- src/layout/components/modals/products.tsx
+++ src/layout/components/modals/products.tsx
@@ -1,11 +1,8 @@
-import * as Yup from 'yup'
-import { FormHandles } from '@unform/core'
-import { Form } from '@unform/web'
-import { useRef, useState } from 'react'
 import Modal from 'react-modal'
 import { formatDateToUTC, KTSVG } from '../../../helpers'
 import { IPartialProductResponse } from '../../../interfaces/api-response/productsPartialResponse'
 import { DatePicker, Select } from '../inputs'
+import { Dispatch, SetStateAction, useState } from 'react'
 
 type NewTransactionModalProps = {
   isOpen: boolean
@@ -13,49 +10,39 @@ type NewTransactionModalProps = {
   message: string
   action: () => Promise<void>
   onRequestClose: () => void
-  onAddProduct: React.Dispatch<React.SetStateAction<IPartialProductResponse[]>>
+  onAddProduct: Dispatch<SetStateAction<IPartialProductResponse[]>>
 }
 
 export function ProductsModal({ isOpen, modalTitle, message, action, onRequestClose, onAddProduct }: NewTransactionModalProps) {
-  const formRef = useRef<FormHandles>(null)
+  const [grantedProducts, setGrantedProducts] = useState([
+    {
+      id: '1',
+      name: 'Day Trade - Do básico ao avançado',
+      expireDate: '26/10/2022',
+      type: 'Cursos'
+    },
+    {
+      id: '2',
+      name: 'Análises de Criptomoedas',
+      expireDate: '26/10/2022',
+      type: 'Cursos'
+    },
+    {
+      id: '3',
+      name: 'Segue o gráfico - Mensal',
+      expireDate: '26/10/2022',
+      type: 'Cursos'
+    },
+  ])
 
-  const [defaultValue, setDefaultValue] = useState({})
-
-  async function handleGrantProduct(data: IPartialProductResponse) {    
-    if (!formRef.current) throw new Error()   
-
-    try {      
-      formRef.current.setErrors({})
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome é Necessário'),
-        expireDate: Yup.string().required('Data de nascimento é necessária')
-      })
-      await schema.validate(data, { abortEarly: false })
-      console.log(data)
-
-      const dataToSend = formatDataToSend(data)
-      // handleCreateUser(dataToSend)
-    } catch (err) {
-      const validationErrors = {}
-      if (err instanceof Yup.ValidationError) {
-        err.inner.forEach((error) => {
-          // @ts-ignore
-          validationErrors[error.path] = error.message
-        })
-        formRef.current.setErrors(validationErrors)
-      }
-    }
-  }
-
-  function formatDataToSend(data: any) {
-    const userData = {
-      id,
-      name: data.name,
-      expireDate: formatDateToUTC(data.birthDate).toISOString().split('T')[0],
-    }      
-
-    return userData
-  }
+  const [selectedProducts, setSelectedProducts] = useState([
+    {
+      id: '1',
+      name: 'Day Trade - Do básico ao avançado',
+      expireDate: '26/10/2022',
+      type: 'Cursos'
+    },
+  ])
 
   return (
     <Modal
@@ -78,27 +65,43 @@ export function ProductsModal({ isOpen, modalTitle, message, action, onRequestCl
           </div>
 
           <div className='modal-body'>
-            <div>
-              <Form className='form d-flex align-items-center gap-20 w-100' ref={formRef} initialData={defaultValue} onSubmit={handleGrantProduct}>
-                <div className='w-25'>
-                  <Select name='courses' label='Cursos'>
-                    <option value='Curso 1'>
-                      Curso 1
-                    </option>
-                  </Select>
+            {selectedProducts && (
+              selectedProducts.map(product => (
+                <div key={product.id} className='d-flex align-items-center gap-18'>
+                  <div className='w-25'>
+                    <Select name='courses' label={product.type} fixedValue={product.name} disabled>
+                      <option>
+                        {product.name}
+                      </option>
+                    </Select>
+                  </div>
+                  <DatePicker name='expireDate' label='Data de expiração' />
                 </div>
-                <DatePicker name='expireDate' label='Data de expiração' />
-                <button type='submit' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75'>
-                  + Adicionar outro curso
-                </button>
-              </Form>
+              ))
+            )}
+            <div className='d-flex align-items-center gap-20'>
+              <div className='w-25'>
+                <Select name='courses' label='Cursos'>
+                  {grantedProducts.map(product => (
+                    <option key={product.id} value={product.name}>
+                      {product.name}
+                    </option>
+                  ))}
+                </Select>
+              </div>
+              <DatePicker name='expireDate' label='Data de expiração' />
+              <button type='submit' className='btn btn-outline-primary btn-sm border border-primary w-25 h-75'>
+                + Adicionar outro curso
+              </button>
             </div>
             <div className='d-flex align-items-center gap-20'>
               <div className='w-25'>
                 <Select name='trainings' label='Treinamentos'>
-                  <option value='Treinamento 1'>
-                    Treinamento 1
-                  </option>
+                  {grantedProducts.map(product => (
+                    <option key={product.id} value={product.name}>
+                      {product.name}
+                    </option>
+                  ))}
                 </Select>
               </div>
               <DatePicker name='expireDate' label='Data de expiração' />
@@ -109,9 +112,11 @@ export function ProductsModal({ isOpen, modalTitle, message, action, onRequestCl
             <div className='d-flex align-items-center gap-20'>
               <div className='w-25'>
                 <Select name='plans' label='Planos'>
-                  <option value='Plano 1'>
-                    Plano 1
-                  </option>
+                  {grantedProducts.map(product => (
+                    <option key={product.id} value={product.name}>
+                      {product.name}
+                    </option>
+                  ))}
                 </Select>
               </div>
               <DatePicker name='expireDate' label='Data de expiração' />
