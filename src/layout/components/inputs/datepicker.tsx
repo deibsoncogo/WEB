d8b987e2f404a470fc74f5bc88470e7694adfc01
Solenir <solenir.figueredo@gmail.com>
2022-06-16 11:16:58 -0300

    refactor: added mask to datePicker

--- src/layout/components/inputs/datepicker.tsx
+++ src/layout/components/inputs/datepicker.tsx
@@ -1,43 +1,46 @@
 import { useRef, useState, useEffect } from 'react'
-import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker'
+import ReactDatePicker, { ReactDatePickerProps, registerLocale } from 'react-datepicker'
+import br from 'date-fns/locale/pt-BR'
 import { useField } from '@unform/core'
 
-import { formatDateToUTC, rangeInt } from '../../../helpers'
+import { rangeInt } from '../../../helpers'
 import { months } from '../../../utils/months'
 
 import 'react-datepicker/dist/react-datepicker.css'
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
+import { InputMasked } from './input-mask'
 
 interface Props extends Omit<ReactDatePickerProps, 'onChange'> {
   name: string
   label: string
+  mask?: string
   classes?: string
 }
 
-export function DatePicker({ name, label, classes, ...rest }: Props) {
+registerLocale('br', br)
+export function DatePicker({ name, label, mask, classes, ...rest }: Props) {
   const datepickerRef = useRef(null)
-  const { fieldName, registerField, defaultValue, error } = useField(name)
+  const { fieldName, registerField, defaultValue, error, clearError } = useField(name)
 
-  const [date, setDate] = useState(defaultValue || null)
+  const [date, setDate] = useState(defaultValue || undefined)
 
   useEffect(() => {
     registerField({
-      ref: datepickerRef,
       name: fieldName,
+      ref: datepickerRef,
       getValue: (ref) => {
-        return ref.current?.props.selected
+        return ref.current.props.selected
       },
-      setValue: (ref, newValue) => {
-        ref.current.value = newValue
-        const newDate = formatDateToUTC(newValue as string)
-        setDate(newDate)
+      setValue: (ref: any, value: string) => {
+        setDate(value)
+        ref.current.value = value
       },
-      clearValue: (ref) => {
-        ref.current.value = ''
+      clearValue: () => {
+        setDate(undefined)
       },
     })
   }, [fieldName, registerField])
-  
+
   const years = rangeInt(1900, new Date().getFullYear() + 1)
 
   return (
@@ -47,60 +50,64 @@ export function DatePicker({ name, label, classes, ...rest }: Props) {
           {label}
         </label>
       )}
-      <ReactDatePicker
-        ref={datepickerRef}
-        className='form-control bg-secondar'
-        selected={date}
-        onChange={setDate}        
-        dateFormat='dd/MM/yyyy'
-        placeholderText={error && error}
-        {...rest}
-        renderCustomHeader={({
-          date,
-          changeYear,
-          changeMonth,
-          decreaseMonth,
-          increaseMonth,
-          prevMonthButtonDisabled,
-          nextMonthButtonDisabled,
-        }) => (
-          <div className='m-5 d-flex justify-content-center'>
-            <button type='button' onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
-              <AiOutlineLeft />
-            </button>
 
-            <select
-              value={date.getFullYear()}
-              onChange={({ target: { value } }) => changeYear(parseInt(value))}
-            >
-              <option value='' hidden disabled selected>
-                {error ? error : 'Selecione'}
-              </option>
-              {years.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
+      <div>
+        <ReactDatePicker
+          ref={datepickerRef}
+          className='form-control bg-secondar'
+          selected={date}
+          onChange={setDate}
+          dateFormat='dd/MM/yyyy'
+          name={name}
+          locale='br'
+          onFocus={clearError}
+          {...rest}
+          customInput = {<InputMasked name={name} mask={mask ? mask: ''}/>}
+          renderCustomHeader={({
+            date,
+            changeYear,
+            changeMonth,
+            decreaseMonth,
+            increaseMonth,
+            prevMonthButtonDisabled,
+            nextMonthButtonDisabled,
+          }) => (
+            <div className='m-5 d-flex justify-content-center'>
+              <button type='button' onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+                <AiOutlineLeft />
+              </button>
 
-            <select
-              value={months[date.getMonth()]}
-              onChange={({ target: { value } }) => changeMonth(months.indexOf(value))}
-            >
-              {months.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
+              <select
+                value={date.getFullYear()}
+                onChange={({ target: { value } }) => changeYear(Number(value))}
+              >
+                {years.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
 
-            <button type='button' onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-              <AiOutlineRight />
-            </button>
-          </div>
-        )}
-        {...rest}
-      />
+              <select
+                value={months[date.getMonth()]}
+                onChange={({ target: { value } }) => changeMonth(months.indexOf(value))}
+              >
+                {months.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+
+              <button type='button' onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+                <AiOutlineRight />
+              </button>
+            </div>
+          )}
+          {...rest}
+        />
+      </div>
+      {error && <span className='text-danger'>{error}</span>}
     </div>
   )
 }
