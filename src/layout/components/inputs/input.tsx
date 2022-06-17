017f207451ec653faeabdfc4edd23a9453c3c8ca
Solenir <solenir.figueredo@gmail.com>
2022-06-16 23:34:28 -0300

    refactor: added message erro bellow the item

--- src/layout/components/inputs/input.tsx
+++ src/layout/components/inputs/input.tsx
@@ -55,7 +55,7 @@ export function Input({ name, label, placeholderText, classes, onChange, ...rest
       {name != 'content' ? (
         <div className='form-control d-flex align-items-center form-control-lg bg-secondary p-0 m-0 border-0'>
           <input
-            className={`form-control form-control-lg form-control-solid border-transparent bg-secondary ${error && 'placeholder-red'}`}
+            className='form-control form-control-lg form-control-solid border-transparent bg-secondary'
             type='text'
             name={name}
             placeholder={error ? error : placeholderText}
@@ -65,6 +65,7 @@ export function Input({ name, label, placeholderText, classes, onChange, ...rest
             onChangeCapture={clearError}
             {...rest}
           />
+         
 
           {rest.type === 'password' && isEyeVisible && (
             <AiFillEye size={24} className='me-2' onClick={switchType} />
@@ -84,6 +85,8 @@ export function Input({ name, label, placeholderText, classes, onChange, ...rest
           {...rest}
         />
       )}
+      {error && <span className='text-danger'>{error}</span>}
     </div>
+     
   )
 }
