8dbc9074e863d255697de5d57766b535e62c787d
Gustavo França <gstvz.franca@gmail.com>
2022-06-23 14:15:22 -0300

    feat: implemented phoneFormatter to format phone numbers

---
+++ src/layout/formatters/phoneFormatter.ts
@@ -0,0 +1,12 @@
+export function phoneMask(value: string) {
+  if (!value) {
+    return ''
+  }
+
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1)$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+    .replace(/(-\d{4})\d+?$/, '$1')
+}
