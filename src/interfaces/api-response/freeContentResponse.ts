ad52e734ec2ac258963c2faed1757dd0ba67fe01
Solenir <solenir.figueredo@gmail.com>
2022-07-11 16:42:26 -0300

    feat: added interface for free content response

---
+++ src/interfaces/api-response/freeContentResponse.ts
@@ -0,0 +1,11 @@
+
+export interface IFreeContentResponse{
+    id: string;
+    title: string;    
+    description: string
+    contentType: string 
+    link?: string       
+    articleContent?: string
+    imageUrl?: string;
+     
+}
