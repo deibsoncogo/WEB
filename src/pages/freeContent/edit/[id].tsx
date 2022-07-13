52f72015a6b82e5955cf0b4fa12e50f58c5d0511
Solenir <solenir.figueredo@gmail.com>
2022-07-13 16:22:38 -0300

    feat: added initial page for update free content

---
+++ src/pages/freeContent/edit/[id].tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { AsideDefault } from '../../../layout/components/aside/AsideDefault'
+import { HeaderWrapper } from '../../../layout/components/header/HeaderWrapper'
+
+const EditFreeContent: NextPage = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <>
+      <Head>
+        <title>Editar Conteúdo Gratuito</title>
+      </Head>
+
+      <div className='page d-flex flex-row flex-column-fluid'>
+        <AsideDefault />
+
+        <div className='wrapper d-flex flex-column flex-row-fluid' id='kt_wrapper'>
+          <HeaderWrapper title='Editar Conteúdo' />
+
+          <div id='kt_content_container' className='container'>
+            <div className=' bg-white rounded shadow-sm p-10 p-lg-15 mx-auto'>
+              {/* <MakeFormUpdateCourse id={id as string} /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EditFreeContent
