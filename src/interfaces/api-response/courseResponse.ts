9be1bf20c48661446b67c58ec91def4920502224
Gustavo França <gstvz.franca@gmail.com>
2022-07-15 11:35:08 -0300

    fix: adjusted products modal fetching and select inputs

--- src/interfaces/api-response/courseResponse.ts
+++ src/interfaces/api-response/courseResponse.ts
@@ -1,18 +1,16 @@
-
-export interface ICourseResponse{
-    id: string;
-    name: string;
-    imageUrl: string;
-    description: string;
-    price: string; 
-    installments: string;
-    discount: string;
-    accessTime: string;
-    userId: string;
-    content: string;
-    isActive: boolean;
-    categoryId: string;
-    teacherName: string;  
-    categoryName: string; 
-     
+export interface ICourseResponse {
+  id: string
+  name: string
+  imageUrl: string
+  description: string
+  price: string
+  installments: string
+  discount: string
+  accessTime: string
+  userId: string
+  content: string
+  isActive: boolean
+  categoryId: string
+  teacherName: string
+  categoryName: string
 }
