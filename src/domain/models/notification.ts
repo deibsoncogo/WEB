48b60708808a943b3fddee6f931a55a176785769
Solenir <solenir.figueredo@gmail.com>
2022-07-18 22:50:26 -0300

    feat: added create form for notification

---
+++ src/domain/models/notification.ts
@@ -0,0 +1,7 @@
+
+
+export enum NotificationType {
+  GENERAL = 'Geral',
+  SUBSCRIBER = 'Assinantes',
+}
+
---
+++ src/layout/components/forms/notification/create.tsx
@@ -0,0 +1,76 @@
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web'
+import React from 'react'
+import { Button } from '../../buttons/CustomButton'
+import { DrawerRight } from '../../drawerRight/DrawerRight'
+import { DatePicker, Input, Select, TextArea } from '../../inputs'
+import { notificationTypeOptions } from './notificationTypeOptions'
+
+type Props = {
+  visible: boolean
+  close: () => void
+  loading: boolean
+  handleCreateNotification: () => void
+}
+
+const CreateNotificationDrawer = React.forwardRef<FormHandles, Props>((props, ref) => {
+  const { close, handleCreateNotification, loading, visible } = props
+  return (
+    <DrawerRight title='Nova Notificação' visible={visible} close={close}>
+      <div className='mt-6 d-flex flex-column justify-content-between h-100'>
+        <Form className='form' ref={ref} onSubmit={handleCreateNotification} id='create-notification-form'>
+          <Input name='tag' label='Tag' type='text' />
+          <TextArea
+              name='text'
+              label='Texto'
+              style={{ minHeight: '150px', margin: 0 }}
+            />
+             <DatePicker
+              name='date'
+              label='Data'
+              placeholderText='00/00/0000'
+            />
+
+            <Select
+              name='notificationType'
+              label='Tipo'
+              classes='h-75px'
+              defaultValue=''             
+            >
+              <option disabled value=''>
+                Selecione
+              </option>
+              {notificationTypeOptions.map(({ label, value }) => (
+                <option value={value} key={value}>
+                  {label}
+                </option>
+              ))}
+            </Select>
+
+        </Form>
+
+
+        <div className='d-flex mb-15'>
+          <Button
+            title='Cancelar'
+            type='button'
+            disabled={loading}
+            onClick={close}
+            customClasses={['btn-secondary', 'px-20', 'ms-auto', 'me-10']}
+          />
+
+          <Button
+            type='submit'
+            form='create-category-form'
+            customClasses={['px-20', 'btn-primary']}
+            title='Salvar'
+            loading={loading}
+          />
+        </div>
+      </div>
+    </DrawerRight>
+  )
+})
+CreateNotificationDrawer.displayName = 'CreateNotificationDrawer'
+
+export { CreateNotificationDrawer }
