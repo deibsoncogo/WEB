e4e53a3448fa1d669205be97d6a51f71ea6118a8
Gustavo França <gstvz.franca@gmail.com>
2022-07-21 09:55:13 -0300

    fix: user knowledge level is now sent with the form

--- src/layout/components/forms/user/edit-user.tsx
+++ src/layout/components/forms/user/edit-user.tsx
@@ -125,6 +125,7 @@ export function FormEditUser({
         : null,
       phoneNumber: phoneNumber || null,
       role: data.role,
+      level: data.level,
       address: [
         {
           zipCode: zipCode || null,
@@ -253,7 +254,7 @@ export function FormEditUser({
               classes='h-75px'
               name='birthDate'
               label='Data de Nascimento'
-              maxDate={new Date()}
+              maxDate={new Date('01/01/2003')}
             />
             <InputMasked
               classes='h-75px'
