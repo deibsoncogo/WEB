3da0153be81d6c9d05b3750d1bcb331cbd2a569a
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-08-30 09:04:17 -0300

    fix: adjustments to genarate build

--- src/application/factories/usecases/chatTraining/remote-createChatTraining-factory.ts
+++
@@ -1,7 +0,0 @@
-import { ICreateChatTraining } from './../../../../domain/usecases/interfaces/chatTraining/createTraining';
-import { makeAxiosHttpClient } from './../../http/axiosHttpClient-factory';
-import { makeApiUrl } from "../../http";
-import { RemoteCreateChatTraining } from '../../../../data/usecases/chatTraining/remote-createChatTraining';
-
-export const makeRemoteCreateChatTraining = (): ICreateChatTraining =>
-     new RemoteCreateChatTraining(makeApiUrl('/chatTraining'), makeAxiosHttpClient());
