338c776418ad92470bcc05c52fa742230ad72e4f
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-09-06 13:06:35 -0400

    fix: InvalidCredentialErrors message

--- src/data/usecases/user/remote-userSignIn.ts
+++ src/data/usecases/user/remote-userSignIn.ts
@@ -1,27 +1,33 @@
 import { UnexpectedError } from '../../../domain/errors/unexpected-error'
 import { HttpClient, HttpStatusCode } from '../../protocols'
-import { AccessDeniedError, InvalidCredentialsError, InvalidParamsError } from '../../../domain/errors'
+import {
+  AccessDeniedError,
+  InvalidCredentialsError,
+  InvalidParamsError,
+} from '../../../domain/errors'
 import { IUserSignIn } from '../../../domain/usecases/interfaces/user/userSignIn'
 import { UserSignIn } from '../../../domain/models/userSignIn'
 import { IAuthResponse } from '../../../interfaces/api-response/authResponse'
 
-
 export class RemoteUserSignIn implements IUserSignIn {
-  constructor(private readonly url: string, private readonly httpClient: HttpClient<IAuthResponse>) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<IAuthResponse>
+  ) {}
 
   async signIn(userSignIn: UserSignIn) {
     const httpResponse = await this.httpClient.request({
       url: this.url,
       method: 'post',
       body: userSignIn,
-    })  
+    })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         return httpResponse.body
       case HttpStatusCode.badRequest:
-        throw new InvalidCredentialsError("Email ou senha inválida.")
-      case HttpStatusCode.unauthorized:        
-        throw new AccessDeniedError("Acesso negado! Você não tem permissão para acessar o sistema.")
+        throw new InvalidCredentialsError('E-mail e/ou senha inválidos.')
+      case HttpStatusCode.unauthorized:
+        throw new AccessDeniedError('Acesso negado! Você não tem permissão para acessar o sistema.')
       default:
         throw new UnexpectedError()
     }
