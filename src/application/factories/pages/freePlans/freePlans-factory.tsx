4c8e35511c959c9cf069ac67ba85be9f81bea31b
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-10-10 15:48:25 -0300

    feat: free plans adjustments

--- src/application/factories/pages/freePlans/freePlans-factory.tsx
+++ src/application/factories/pages/freePlans/freePlans-factory.tsx
@@ -1,7 +1,7 @@
 import { ListFreePlansTemplate } from '../../../../layout/templates/freePlans/list'
 import { makeRemoteDeleteFreePlan } from '../../usecases/freePlan/remote-deleteFreePlan-factory'
 import { makeRemoteGetFreePlans } from '../../usecases/freePlan/remote-getFreePlans-factory'
-import { makeRemoteToggleFreePlanStatus } from '../../usecases/freePlan/remote-toggleFreePlanStatus-factory copy'
+import { makeRemoteToggleFreePlanStatus } from '../../usecases/freePlan/remote-toggleFreePlanStatus-factory'
 
 export const MakeListFreePlansPage = () => {
   return (
