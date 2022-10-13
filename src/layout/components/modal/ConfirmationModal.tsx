0d784907b6a0fa3af20b45a9bfd86ff44e8f04af
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-10-13 17:55:38 -0400

    refactor: change modal close buttons

--- src/layout/components/modal/ConfirmationModal.tsx
+++ src/layout/components/modal/ConfirmationModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Modal } from 'react-bootstrap'
+import { KTSVG } from '../../../helpers'
 import { Button } from '../buttons/CustomButton'
 
 type ConfirmationModalProps = {
@@ -20,9 +21,16 @@ function ConfirmationModal({
 }: ConfirmationModalProps) {
   return (
     <Modal show={isOpen} onHide={onRequestClose} className='mt-20'>
-      <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
-      </Modal.Header>
+      <div className='modal-header'>
+        <h5 className='modal-title'>{title}</h5>
+        <button
+          className='btn btn-icon btn-sm btn-active-light-primary ms-2'
+          aria-label='Close'
+          onClick={onRequestClose}
+        >
+          <KTSVG path='/icons/arr061.svg' className='svg-icon svg-icon-2x' />
+        </button>
+      </div>
 
       <Modal.Body>{content}</Modal.Body>
 
