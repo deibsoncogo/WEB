1a3a82b4368acd7f594ca5700d2345f94b8d53de
Tiago Grossi Hasuda <tiago.ghasuda@gmail.com>
2022-10-19 16:34:15 -0300

    fix: removed PaymentType from product

--- src/layout/components/tables/plans-list/row.tsx
+++ src/layout/components/tables/plans-list/row.tsx
@@ -1,25 +1,16 @@
 import { Tooltip } from '@nextui-org/react'
 import Link from 'next/link'
-import { IPlan, PlanType } from '../../../../domain/models/plan'
+import { IPlan } from '../../../../domain/models/plan'
 import { ITogglePlanStatusParams } from '../../../../domain/usecases/interfaces/plan/togglePlanStatus'
 import { KTSVG } from '../../../../helpers'
 import { maskedToMoney } from '../../../formatters/currenceFormatter'
 import { Switch } from '../../inputs'
 
-enum PaymentType {
-  RECURRING_PAYMENT = 'recurring_payment',
-  SINGLE_PAYMENT = 'single_payment',
-}
-
-const paymentTypeMap = {
-  single_payment: 'Anual',
-  recurring_payment: 'Mensal',
-}
-
 type PlanTableRowProps = {
   plan: IPlan
   togglePlanStatus: (params: ITogglePlanStatusParams) => void
 }
+
 const PlanTableRow = ({ plan, togglePlanStatus }: PlanTableRowProps) => {
   const handlePlanStatusChange = () => {
     togglePlanStatus({ id: String(plan.id) })
