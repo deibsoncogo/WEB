90c7f5cdb41da4e7f3d3f1e7102ab84c3352ce06
Solenir <solenir.figueredo@gmail.com>
2022-10-21 12:48:30 -0300

    refactor: adjusted cep formatter

--- src/layout/formatters/cepFormatter.ts
+++ src/layout/formatters/cepFormatter.ts
@@ -4,8 +4,6 @@ export function cepMask (value: string) {
     }
     return value
       .replace(/\D/g, '')
-      .replace(/(\d{2})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d)/, '$1$2')
-      .replace(/(\d{3})(\d)/, '$1-$2')
+      .replace(/(\d{5})(\d)/, '$1-$2')
   }
   
